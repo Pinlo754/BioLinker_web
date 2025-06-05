fb0f5bf535c20ba4881f73610fe570f680bbdd5f
Pinlo754 <pinlo752004@gmail.com>
2025-06-06 01:36:10 +0700

    first commit

---
+++ src/pages/About/AboutScreen.tsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+  <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <h1 className="text-2xl">Giới thiệu</h1>
+    <p className="mt-4 text-gray-700">
+      Đây là trang giới thiệu của ứng dụng BioLinker. 
+    </p>
+  </div>
+  );
+};
+
+export default About;
