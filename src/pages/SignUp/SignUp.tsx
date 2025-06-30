6009c3a25351af8b72b2f663054db067728f290e
Minh Khiêm <phamminhkhiem16@gmail.com>
2025-06-30 16:01:09 +0700

    Ui footer

---
+++ src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/sections/Header'
+import Footer from '../../components/sections/footer'
+
+function SignUp() {
+  return (
+    <div className="bg-gray-100 min-h-screen flex flex-col">
+        <Header/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default SignUp
