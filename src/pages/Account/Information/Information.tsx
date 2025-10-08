3cd92294bf46700c669eb5e5f8f27d8f28a98f96
Minh Khiêm <phamminhkhiem16@gmail.com>
2025-10-08 15:48:39 +0700

    translate account to Vietnamese

--- src/pages/Account/Information/Information.tsx
+++ src/pages/Account/Information/Information.tsx
@@ -30,7 +30,7 @@ const Information = () => {
                 {loading && <LoadingOverlay visible={loading} message="Saving details..." />}
                 {/* personal information */}
                 <div className="w-full gap-10 text-2xl mt-6 text-black">
-                    <span className="text-2xl font-bold ">My Information</span>
+                    <span className="text-2xl font-bold ">Thông tin cá nhân của tôi</span>
                     <div className="flex flex-col rounded-3xl py-8 px-6 shadow-emerald-500/50  shadow-2xl mt-8 bg-white">
 
                         {/* input field */}
@@ -63,7 +63,7 @@ const Information = () => {
                                     id="name"
                                     type="text"
                                     className="peer block w-full border-b border-gray-300 bg-transparent pt-4 pb-2 text-base text-gray-900 placeholder-transparent focus:border-green2 focus:border-b focus:ring-green2 focus:outline-none"
-                                    placeholder="Display Name"
+                                    placeholder="Tên hiển thị"
                                     value={name}
                                     onChange={(e) => setName(e.target.value)}
                                 />
@@ -74,7 +74,7 @@ const Information = () => {
                                     peer-focus:top-0 peer-focus:-translate-y-[65%] peer-focus:scale-75 peer-focus:text-green2
                                     peer-[&:not(:placeholder-shown)]:top-0 peer-[&:not(:placeholder-shown)]:-translate-y-[70%] peer-[&:not(:placeholder-shown)]:scale-75"
                                 >
-                                    Display Name
+                                    Tên hiển thị
                                 </label>
                             </div>
 
@@ -86,7 +86,7 @@ const Information = () => {
                                     className="peer block w-full border-b border-gray-300 bg-transparent pt-4 pb-2 text-base text-gray-900 placeholder-transparent focus:border-green2 focus:border-b focus:ring-green2 focus:outline-none"
                                     value={description}
                                     onChange={(e) => setDescription(e.target.value)}
-                                    placeholder="Description"
+                                    placeholder="Mô tả về bản thân"
                                 />
                                 <label
                                     htmlFor="description"
@@ -95,39 +95,39 @@ const Information = () => {
                                     peer-focus:top-0 peer-focus:-translate-y-[65%] peer-focus:scale-75 peer-focus:text-green2
                                     peer-[&:not(:placeholder-shown)]:top-0 peer-[&:not(:placeholder-shown)]:-translate-y-[70%] peer-[&:not(:placeholder-shown)]:scale-75"
                                 >
-                                    Description
+                                    Mô tả về bản thân
                                 </label>
                             </div>
                             
-                            <span className="text-base font-medium text-start">Your email can`t be changed as you signed up using your Google account.</span>
+                            <span className="text-base font-medium text-start">Email của bạn không thể được thay đổi vì bạn đã đăng ký bằng tài khoản Google.</span>
                         </div>
                     </div>
                     <button className="bg-[#EBEEF1] rounded-3xl px-6 py-4 h-[65px] font-semibold mt-6 text-[#4F4F4F]"
                             onClick={handleSaveDetails}>
-                        Save Details
+                        Lưu thông tin
                     </button>                   
                 </div>
                 
                 {/* change password */}
                 <div className="w-full gap-10 text-2xl mt-6">
-                    <span className="text-2xl font-bold">Security and Privacy</span>
+                    <span className="text-2xl font-bold">Bảo mật và Quyền riêng tư</span>
                     <div className="flex flex-col rounded-3xl py-8 px-6 shadow-emerald-500/50 bg-white shadow-2xl mt-8">
                         <span className="text-xl font-medium text-start">
-                            Change your password
+                            Thay đổi mật khẩu
                         </span>
                         <span className="text-base text-start mt-2">
-                            You can change your password by clicking the button below.
+                            Bạn có thể thay đổi mật khẩu bằng cách nhấn nút bên dưới.
                         </span>
                         <button className="rounded-3xl border px-6 py-4 h-[65px] font-semibold mt-6 text-[#4F4F4F] hover:bg-[#EBEEF1]">
-                            Change Password
+                            Thay đổi mật khẩu
                         </button>
                     </div>
                 </div>
 
                 {/*Account management*/}
                 <div className="w-full gap-10 text-2xl mt-6">
-                    <span className="text-2xl font-bold">Account management</span>
-                    <span className="text-xl font-medium text-start mt-2">BioLinker you own</span>
+                    <div className="text-2xl font-bold">Quản lý tài khoản</div>
+                    <span className="text-xl font-medium text-start mt-2">BioLinker là tài khoản của bạn</span>
                     <div className="flex flex-row rounded-3xl py-8 px-6 shadow-emerald-500/50 bg-white shadow-2xl mt-8">
                         {/* side bar image */}
                         <button className="border-r border-black pr-6 w-[20%] flex items-start justify-center" onClick={() => fileInputRef.current?.click()}>
@@ -151,11 +151,11 @@ const Information = () => {
                                 <span className="text-base text-start">{username}</span>
                             </div>
                             <div className="px-6 py-8 border-b border-black gap-4 text-base text-start font-bold">
-                                <div className="">Plan</div>
-                                <span className="">{plan}</span>
+                                <div className="">Gói dịch vụ:</div>
+                                <span className="text-green2">{plan === "FreeUser" ? "Miễn phí" : "Nâng cao"}</span>
                             </div>
                             <div className="px-6 py-10 text-center text-base font-bold border-black flex flex-col items-center justify-center">
-                                <span className="">Upgrade to Pro to unlock more function in BioLinker</span>
+                                <span className="">Nâng cấp lên gói Nâng cao để mở khóa thêm chức năng trong BioLinker</span>
                                 <button className="align-center gap-4 justify-center flex flex-row rounded-3xl border px-6 py-4 w-[70%] font-semibold mt-2 text-[#4F4F4F]  bg-gradient-to-r from-green1 to-green2 text-white">
                                     <span className="text-base text-white font-semibold">Upgrade to Pro</span>
                                 </button>
@@ -168,13 +168,13 @@ const Information = () => {
                 <div className="w-full gap-10 text-2xl mt-6">       
                     <div className="flex flex-col rounded-3xl py-8 px-6 shadow-emerald-500/50 bg-white shadow-2xl mt-8">
                         <span className="text-xl font-medium text-start">
-                        Delete forever
+                        Xóa tài khoản
                         </span>
                         <span className="text-base text-start mt-2">
-                            Permanently delete your account and all your link-in-bio.
+                            Xóa tài khoản của bạn và tất cả các liên kết trong BioLinker.
                         </span>
                         <button className="rounded-3xl border border-red-500/80 px-6 py-4 h-[65px] font-semibold mt-6 text-red-500/80 hover:bg-red-500/10">
-                            Delete Account
+                            Xóa tài khoản
                         </button>
                     </div>
                 </div>
