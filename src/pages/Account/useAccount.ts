0e0b6d2a2d36e37aa6e89c849605e35962fa306d
Minh Khiêm <phamminhkhiem16@gmail.com>
2025-10-17 17:32:16 +0700

    update plan UI

--- src/pages/Account/useAccount.ts
+++ src/pages/Account/useAccount.ts
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const useAccount = () => {
-    const menuComponent = ["BioLinker", "Thông tin cá nhân", "Cài đặt", "QR của tôi", "Hỗ trợ", "Đăng xuất"];
+    const menuComponent = ["BioLinker", "Thông tin cá nhân","Tên miền", "Nâng cấp", "QR của tôi", "Hướng dẫn", "Đăng xuất"];
     return {
         menuComponent,
     };
